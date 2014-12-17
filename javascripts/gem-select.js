c13cc8be7a0de1a69c26b781dce3750302659f9a
Piotr Solnica <piotr.solnica@gmail.com>
2014-12-17 15:47:49 +0100

    Site updated to 901adc6

---
+++ javascripts/gem-select.js
@@ -0,0 +1,1 @@
+$(function(){var e={SQL:"rom-sql",MongoDB:"rom-mongo"},t=$("#gem-install-code"),n=$("#db-select .value");$("#db-dropdown a").on("click",function(r){var i=$(r.target).text();n.text(i),t.text("$ gem install "+e[i]),r.preventDefault()})});
