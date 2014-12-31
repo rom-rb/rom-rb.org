db4d9cc7e9b703f2a420af1552b13cca99312e8c
Piotr Solnica <piotr.solnica@gmail.com>
2014-12-31 19:06:33 +0100

    Site updated to 4a1bb61

--- javascripts/gem-select.js
+++ javascripts/gem-select.js
@@ -1,1 +1,1 @@
-$(function(){var e={SQL:"rom-sql",MongoDB:"rom-mongo"},t=$("#gem-install-code"),n=$("#db-select .value");$("#db-dropdown a").on("click",function(r){var i=$(r.target).text();n.text(i),t.text("$ gem install "+e[i]),r.preventDefault()})});
+$(function(){var t={SQL:"rom-sql",MongoDB:"rom-mongo"},e=$("#gem-install-code"),o=$("#db-select .value");$("#db-dropdown a").on("click",function(n){var l=$(n.target).text();o.text(l),e.text("$ gem install "+t[l]),n.preventDefault()})});
---
+++ tutorials/code/rom-todo-app-template.rb
@@ -0,0 +1,84 @@
+gem "rom", '~> 0.5'
+gem "rom-sql", '~> 0.3.1'
+gem "rom-rails", '~> 0.2'
+
+gem_group(:test) do
+  gem "rspec"
+  gem "rspec-rails"
+  gem "capybara"
+  gem "database_cleaner"
+  gem "spring-commands-rspec"
+end
+
+application "require 'rom-rails'"
+
+run 'bundle'
+
+generate "rspec:install"
+
+gsub_file "spec/rails_helper.rb",
+  "config.use_transactional_fixtures = true",
+  "config.use_transactional_fixtures = false"
+
+insert_into_file "spec/rails_helper.rb",
+  after: "config.use_transactional_fixtures = false\n" do
+  <<-CONTENT
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  CONTENT
+end
+
+generate "migration", "create_tasks", "title:string"
+
+generate "rom:relation", "tasks"
+generate "rom:mapper", "tasks"
+generate "rom:commands", "tasks"
+
+route "resources :tasks"
+
+rake "db:migrate"
+
+file "spec/features/tasks_spec.rb" do
+  <<-CONTENT.strip_heredoc
+    require 'rails_helper'
+
+    feature 'Tasks' do
+      # examples...
+    end
+  CONTENT
+end
+
+file "spec/relations/tasks_spec.rb" do
+  <<-CONTENT.strip_heredoc
+    require 'rails_helper'
+
+    describe 'Tasks relation' do
+      # examples...
+    end
+  CONTENT
+end
+
+file "spec/fixtures/tasks.yml" do
+  <<-CONTENT.strip_heredoc
+    one:
+      id: 1
+      title: 'Task One'
+    two:
+      id: 2
+      title: 'Task Two'
+  CONTENT
+end
