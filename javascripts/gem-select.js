cd4b832a63f18898159feb74810de124ae7307b1
Don Morrison <don@elskwid.net>
2015-01-27 00:40:10 +0000

    Site updated to d435872

--- javascripts/gem-select.js
+++ javascripts/gem-select.js
@@ -1,1 +1,1 @@
-$(function(){var e={SQL:"rom-sql",MongoDB:"rom-mongo"},t=$("#gem-install-code"),n=$("#db-select .value");$("#db-dropdown a").on("click",function(r){var i=$(r.target).text();n.text(i),t.text("$ gem install "+e[i]),r.preventDefault()})});
+$(function(){var e={SQL:"rom-sql",MongoDB:"rom-mongo",YAML:"rom-yaml",CSV:"rom-csv"},t=$("#gem-install-code"),n=$("#db-select .value");$("#db-dropdown a").on("click",function(r){var i=$(r.target).text();n.text(i),t.text("$ gem install "+e[i]),r.preventDefault()})});
